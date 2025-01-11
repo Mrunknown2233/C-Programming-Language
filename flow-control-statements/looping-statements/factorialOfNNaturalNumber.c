451bee00c700afc282e7f2dc10c691962365a76b
Mrunknown2233 <dedhiasoumya299@gmail.com>
2025-01-11 19:36:35 +0530

    Added the remaining files for singular loop program. The next commit will include the neseted loop programs mostly programs for patterns

---
+++ flow-control-statements/looping-statements/factorialOfNNaturalNumber.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	int i,fact,n;
+
+	printf("\nEnter the value of n : ");
+	scanf("%d",&n);
+
+	fact = 1;
+	for(i = 1; i <=n; i++)
+		fact*=i;
+	
+	printf("\nTh factorial of %d is %d ",n,fact);
+}
